b940a78a9fd1445299fffddcc0372fa632f4cab1
Alexandr Dulya <sasa97973@yandex.ru>
2017-04-30 12:21:34 +0300

    Reworked 19.php, added function date()

--- Backend/29.04.2017/19.php
+++ Backend/29.04.2017/19.php
@@ -1,7 +1,7 @@
 <?php
 
-$arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-$day = 'Воскресенье';
+$arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+$day = date('l');
 foreach ($arr as $item) {
     if ($item == $day) {
         echo " <i>{$item}</i>";
