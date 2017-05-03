0ebecf99ae14115947660851a7b7ffeef871649a
Alexandr Dulya <sasa97973@yandex.ru>
2017-05-03 10:14:06 +0300

    Back 29.04 added reworked 28.php

--- Backend/29.04.2017/28.php
+++ Backend/29.04.2017/28.php
@@ -1,8 +1,12 @@
-<?php
-
-for ($column = 1; $column <= 9; $column++) {
-    for ($row = 1; $row <= 9; $row++) {
-        echo "$column x $row = ". $row * $column .'<br>';
-    }
-    echo '<br>';
-}
+<table border="1" cellpadding="15px">
+    <?php for ($i = 1; $i <= 9; $i++) : ?>
+    <tr>
+        <?php for ($g = 1; $g <= 9; $g++) : ?>
+            <td>
+                <?php $result = $i * $g; ?>
+                <?php echo "{$g} x {$i} = {$result}";?>
+            </td>
+        <?php endfor; ?>
+    </tr>
+<?php endfor; ?>
+</table>
